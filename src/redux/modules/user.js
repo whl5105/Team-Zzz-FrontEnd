b1d4886ccd1f169303e571b1c87522b8e68487fd
siun <in6318008@gmail.com>
2021-12-30 10:44:23 +0900

    [docs] 로그인 스토리지 저장 코드수정

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,25 +1,25 @@
 import { createAction, handleActions } from "redux-actions";
 import { produce } from "immer";
 import { apis } from "../../shared/api/apis";
-import { deleteCookie, setCookie } from "../../shared/Cookie";
+// import { deleteCookie, setCookie } from "../../shared/Cookie";
 
 // -- actions --
 const SIGNUP = "SIGNUP";
-const LOGIN = "LOGIN";
+const SET_USER = "SET_USER";
 
 // -- action creators --
 const signup = createAction(SIGNUP, (errMessage) => ({ errMessage }));
-const setLogin = createAction(LOGIN, (user) => ({ user }));
+const setUser = createAction(SET_USER, (user) => ({ user }));
 
 // -- initialState --
 const initialState = {
-  // user: null,
+  user: null,
   errMessage: "",
   is_login: false,
 };
 
 // -- middleware actions --
-//회원가입 middleware
+//---- 회원가입 DB ----
 export const signupDB =
   (userid, password) =>
   async (dispatch, getState, { history }) => {
@@ -40,11 +40,20 @@ export const loginDB =
     try {
       const res = await apis.login(userId, password);
       let username = res.data[0].userId;
-      setCookie("token", res.data[1].token, 7);
-      localStorage.setItem("username", res.data[0].userId);
-      dispatch(setLogin(res));
+      // setCookie("token", res.data[1].token, 7);
+      //로컬 스토리지 저장
+      localStorage.setItem("userIdx", res.userIdx);
+      localStorage.setItem("noticeSet", res.noticeSet);
+      localStorage.setItem("token", res.token);
+      dispatch(
+        setUser({
+          userIdx: res.userIdx,
+          userId: res.userId,
+          loginCnt: res.loginCnt,
+          noticeSet: res.noticeSet,
+        })
+      );
       window.alert(`${username}님 환영합니다`);
-
       history.replace("/");
     } catch (err) {
       window.alert("없는 회원정보 입니다! 회원가입을 해주세요!");
@@ -59,8 +68,9 @@ export default handleActions(
       produce(state, (draft) => {
         draft.errMessage = action.payload.errMessage;
       }),
-    [LOGIN]: (state, action) =>
+    [SET_USER]: (state, action) =>
       produce(state, (draft) => {
+        draft.user = action.payload.user;
         draft.is_login = true;
       }),
   },
