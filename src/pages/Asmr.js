b88783a1b23f3941d59e61b92594df37a0ed3c0a
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2021-12-31 12:40:03 +0900

    [docs] Asmr.js → 음원 활성화 시 play, 음원 비활성화 시 pause 구현 코드 수정

--- src/pages/Asmr.js
+++ src/pages/Asmr.js
@@ -5,9 +5,11 @@ import { useHistory } from "react-router-dom";
 import { actionCreators as asmrActions } from "../redux/modules/asmr";
 import AsmrPopUp from "../components/AsmrPopUp";
 
+import audioUrl1 from "../audio/audioUrl1.MP3";
+import audioUrl2 from "../audio/audioUrl2.mp3";
+import audioUrl3 from "../audio/audioUrl3.mp3";
 
 const Asmr = ({ location }) => {
-  console.log(location);
   const [getCategory, setCategory] = React.useState(
     location.category ? location.category : "전체"
   );
@@ -15,10 +17,14 @@ const Asmr = ({ location }) => {
   const asmrInfo = useSelector((state) => state.asmr.asmrList);
   const [play, setPlay] = React.useState([]);
 
+  const [song1, setSong1] = React.useState(new Audio());
+  const [song2, setSong2] = React.useState(new Audio());
+  const [song3, setSong3] = React.useState(new Audio());
+
   const history = useHistory();
   const dispatch = useDispatch();
-  const [openModal, setOpenmodal] = React.useState(false)
- console.log(openModal)
+  const [openModal, setOpenmodal] = React.useState(false);
+
   React.useEffect(() => {
     // 1) 카테고리별 활성화 유무
     const arr = ["전체", "자연", "공간", "물체"];
@@ -72,8 +78,6 @@ const Asmr = ({ location }) => {
   }, [getCategory]);
 
   const select = (asmrUrl) => {
-    let playList = []; // 선택된 음원만 play 할 배열
-
     if (play.includes(asmrUrl)) {
       // 비활성화
       let arr = [...play];
@@ -83,7 +87,18 @@ const Asmr = ({ location }) => {
         }
       });
       setPlay(arr);
-      playList = [...arr];
+
+      // 비활성화 시 음원도 해당 음원 재생 정지
+      if (song1.src.indexOf(asmrUrl) !== -1) {
+        song1.pause();
+        setSong1(new Audio());
+      } else if (song2.src.indexOf(asmrUrl) !== -1) {
+        song2.pause();
+        setSong2(new Audio());
+      } else if (song3.src.indexOf(asmrUrl) !== -1) {
+        song3.pause();
+        setSong3(new Audio());
+      }
 
       // 선택한 음원 비활성화 style
       const deleteItem = document.getElementById(asmrUrl);
@@ -92,106 +107,106 @@ const Asmr = ({ location }) => {
       // 활성화
       if (play.length > 2) {
         window.alert("음원은 3개까지만 담으실 수 있습니다.");
-        playList = [...play];
       } else {
         const arr = [...play, asmrUrl];
         setPlay(arr);
-        playList = [...arr];
+
+        // 음원 선택 시 활성화 되면서 음원 재생
+        if (!song1.src) {
+          song1.src = asmrUrl;
+          song1.loop = true;
+          song1.play();
+        } else if (!song2.src) {
+          song2.src = asmrUrl;
+          song2.loop = true;
+          song2.play();
+        } else if (!song3.src) {
+          song3.src = asmrUrl;
+          song3.loop = true;
+          song3.play();
+        }
 
         // 선택한 음원 활성화 style
         const selectItem = document.getElementById(asmrUrl);
         selectItem.style.backgroundColor = "#dddddd";
       }
     }
-
-    console.log(playList.length);
-    // 배열에 있는 음원만 다시 재생....
-    if (playList.length === 0) {
-      console.log("재생 할 거 없음");
-    } else {
-      playList.forEach((item) => {
-        const audioPlayBack = new Audio(item);
-
-        audioPlayBack.loop = true;
-        audioPlayBack.volume = 0.5;
-        audioPlayBack.play(); // 음원 재생
-      });
-    }
   };
 
   return (
     <>
-    <div>  {/* 나중에 여기로 전체 크기 핸드폰 사이즈로 바꿔야함 */}
-      <div
-        onClick={() => {
-          history.push("/");
-        }}
-      >
-        ASMR 페이지
+      <div>
+        {" "}
+        {/* 나중에 여기로 전체 크기 핸드폰 사이즈로 바꿔야함 */}
+        <div
+          onClick={() => {
+            history.push("/");
+          }}
+        >
+          ASMR 페이지
+        </div>
+        <div style={{ display: "flex", margin: "0px 40%" }}>
+          <Category
+            id="전체"
+            onClick={() => {
+              setCategory("전체");
+            }}
+          >
+            전체
+          </Category>
+          <Category
+            id="자연"
+            onClick={() => {
+              setCategory("자연");
+            }}
+          >
+            자연
+          </Category>
+          <Category
+            id="공간"
+            onClick={() => {
+              setCategory("공간");
+            }}
+          >
+            공간
+          </Category>
+          <Category
+            id="물체"
+            onClick={() => {
+              setCategory("물체");
+            }}
+          >
+            물체
+          </Category>
+        </div>
+        <div style={{ margin: "0px 40%", display: "flex", flexWrap: "wrap" }}>
+          {sound.map((item) => {
+            return (
+              <Sound
+                id={item.asmrUrl}
+                key={item.categoryIdx}
+                onClick={() => {
+                  select(item.asmrUrl);
+                }}
+              >
+                <p>{item.asmrUrl}</p>
+                <p>{item.title}</p>
+              </Sound>
+            );
+          })}
+        </div>
+        {play.length > 0 ? (
+          <button
+            onClick={() => {
+              setOpenmodal(true);
+              console.log("음원 url 가지고 이동!!!", play);
+            }}
+          >
+            음량 조절 하러 가기
+          </button>
+        ) : null}
+        {openModal && <AsmrPopUp play={play} closeModal={setOpenmodal} />}
       </div>
-      <div style={{ display: "flex", margin: "0px 40%" }}>
-        <Category
-          id="전체"
-          onClick={() => {
-            setCategory("전체");
-          }}
-        >
-          전체
-        </Category>
-        <Category
-          id="자연"
-          onClick={() => {
-            setCategory("자연");
-          }}
-        >
-          자연
-        </Category>
-        <Category
-          id="공간"
-          onClick={() => {
-            setCategory("공간");
-          }}
-        >
-          공간
-        </Category>
-        <Category
-          id="물체"
-          onClick={() => {
-            setCategory("물체");
-          }}
-        >
-          물체
-        </Category>
-      </div>
-      <div style={{ margin: "0px 40%", display: "flex", flexWrap: "wrap" }}>
-        {sound.map((item) => {
-          return (
-            <Sound
-              id={item.asmrUrl}
-              key={item.categoryIdx}
-              onClick={() => {
-                select(item.asmrUrl);
-              }}
-            >
-              <p>{item.asmrUrl}</p>
-              <p>{item.title}</p>
-            </Sound>
-          );
-        })}
-      </div>
-
-      {play.length > 0 ? (
-        <button
-          onClick={() => {
-            setOpenmodal(true)
-            console.log("음원 url 가지고 이동!!!", play);
-          }}
-        >
-          음량 조절 하러 가기
-        </button>
-      ) : null}
-    { openModal&& <AsmrPopUp play={play} closeModal={setOpenmodal} />}
-    </div>
     </>
   );
 };
