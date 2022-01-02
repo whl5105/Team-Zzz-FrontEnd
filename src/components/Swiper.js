0d207ca0fe6d3fb3bd887030c8334174ee38b0ec
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2022-01-02 13:20:34 +0900

    [style] Swiper 컴포넌트에서 import 부분에 scss를 css로 변경

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Pagination, Autoplay } from "swiper/core";
-import "swiper/scss";
-import "swiper/scss/pagination";
+import "swiper/css";
+import "swiper/css/pagination";
 
 SwiperCore.use([Pagination, Autoplay]);
 
