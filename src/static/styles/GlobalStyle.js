69d13acc35d087705597cfb90b54bc8f087dc87a
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2022-01-03 23:21:17 +0900

    [css] 폰트 수정

--- src/static/styles/GlobalStyle.js
+++ src/static/styles/GlobalStyle.js
@@ -2,12 +2,12 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300;1,400;1,500&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap');
 
 @font-face {
   font-family: 'Roboto', sans-serif;
-  src: url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300;1,400;1,500&display=swap')
+  src: url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap')
     format("woff");
   font-weight: normal;
   font-style: normal;
@@ -16,7 +16,7 @@ const GlobalStyle = createGlobalStyle`
 
 @font-face {
   font-family: 'Noto Sans KR', sans-serif;
-  src: url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500&display=swap')
+  src: url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap')
     format("woff");
   font-weight: normal;
   font-style: normal;
