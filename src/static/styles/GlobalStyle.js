05aa823ae670f809f0532fe79a885d02a6d1495f
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2022-01-03 23:13:08 +0900

    [css] Roboto, Noto Sans KR 폰트 적용

--- src/static/styles/GlobalStyle.js
+++ src/static/styles/GlobalStyle.js
@@ -1,9 +1,32 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300;1,400;1,500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500&display=swap');
+
+@font-face {
+  font-family: 'Roboto', sans-serif;
+  src: url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300;1,400;1,500&display=swap')
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+  unicode-range: U+0041-005A, U+0061-007A;
+}
+
+@font-face {
+  font-family: 'Noto Sans KR', sans-serif;
+  src: url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500&display=swap')
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+  unicode-range: U+AC00-U+D7A3;
+}
+
   * {
     margin: 0;
     padding: 0;  
+    font-family: 'Roboto', 'Noto Sans KR', sans-serif;
   }
 
   body {
