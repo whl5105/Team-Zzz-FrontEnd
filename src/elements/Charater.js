03780dd4e99e272aa88ecb28a5cbd9c9e0493bc9
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2022-01-04 10:31:58 +0900

    [add] character 폴더 안에 이미지 png → svg로 변경

--- src/elements/Charater.js
+++ src/elements/Charater.js
@@ -35,7 +35,7 @@ const Charater = (props) => {
           onClick={_onClick}
           data-value={props.feelNumber}
           name={name}
-          src={require(`../static/images/character/feel${props.feelNumber}.png`)}
+          src={require(`../static/images/character/feel${props.feelNumber}.svg`)}
         ></ImgIcon>
         {children}
       </div>
@@ -51,7 +51,7 @@ const Charater = (props) => {
           data-value={props.sleepNumber}
           // name="sleepScore"
           name={name}
-          src={require(`../static/images/character/sleep${props.sleepNumber}.png`)}
+          src={require(`../static/images/character/sleep${props.sleepNumber}.svg`)}
         ></ImgIcon>
         {children}
       </div>
@@ -67,14 +67,14 @@ const Charater = (props) => {
             onClick={_onClick}
             style={{ zIndex: "3" }}
             name={name}
-            src={require(`../static/images/character/feel${props.feelNumber}.png`)}
+            src={require(`../static/images/character/feel${props.feelNumber}.svg`)}
           />
           <ImgIcon
             {...styles}
             onClick={_onClick}
             style={{ zIndex: "2" }}
             name={name}
-            src={require(`../static/images/character/sleep${props.sleepNumber}.png`)}
+            src={require(`../static/images/character/sleep${props.sleepNumber}.svg`)}
           />
         </IconBox>
       </React.Fragment>
@@ -87,7 +87,7 @@ const Charater = (props) => {
 //-- defaultProps --
 Charater.defaultProps = {
   shape: "circle",
-  src: "../static/images/character/sleep0.png",
+  src: "../static/images/character/sleep0.svg",
   size: 24,
   _onClick: () => {},
   children: null,
