d2cc0925d1f2654fd0d0ade1712692a56a7a7dce
taeeon kim <myhom43@gmail.com>
2022-01-06 20:39:50 +0900

    [docs] 서비스워커 푸시알림 중간 단계

--- src/index.js
+++ src/index.js
@@ -24,3 +24,18 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// navigator.serviceWorker.register('service-worker.js')
+// .then(function(registration) {
+//   return registration.pushManager.getSubscription()
+//   .then(async function(subscription) {
+//       // registration part
+//   });
+// })
+// .then(function(subscription) {
+//     // subscription part
+//     if(subscription) {
+//       return subscription;
+//   }
+// });
+
