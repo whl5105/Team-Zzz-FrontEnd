74836a868ae1a3dcfcd77ff5471d364df3d60825
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2022-01-13 15:38:00 +0900

    [docs] MixListPopUP 수정

--- src/redux/modules/asmr.js
+++ src/redux/modules/asmr.js
@@ -24,9 +24,27 @@ const set_write = createAction(SET_WRITE, () => ({}));
 // -- initialState --
 const initialState = {
   asmrList: [],
-  playList: [
+  playList: [ // 초기값 무조건 null로
     {
       mixIdx: "2",
+      mixTitle: "음원 재생한다",
+      mix1: {
+        asmrUrl:
+          "https://zzz-asmr-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%8B%E1%85%B3%E1%86%B7%E1%84%8B%E1%85%AF%E1%86%AB/%E1%84%80%E1%85%A9%E1%86%BC%E1%84%80%E1%85%A1%E1%86%AB/%E1%84%80%E1%85%A9%E1%86%BC%E1%84%8B%E1%85%AF%E1%86%AB.mp3",
+        sound: "22",
+        iconUrl: "사진",
+        title: "공원",
+      },
+      mix2: {
+        asmrUrl:
+          "https://zzz-asmr-bucket.s3.ap-northeast-2.amazonaws.com/%E1%84%8B%E1%85%B3%E1%86%B7%E1%84%8B%E1%85%AF%E1%86%AB/%E1%84%80%E1%85%A9%E1%86%BC%E1%84%80%E1%85%A1%E1%86%AB/%E1%84%83%E1%85%A9%E1%84%89%E1%85%A5%E1%84%80%E1%85%AA%E1%86%AB.mp3",
+        sound: "22",
+        iconUrl: "사진",
+        title: "도서관",
+      },
+    },
+    {
+      mixIdx: "3",
       mixTitle: "음원 재생한다",
       mix1: {
         asmrUrl:
@@ -86,7 +104,6 @@ const getPlayListDB = () => {
     try {
       const userIdx = localStorage.getItem("userIdx");
       const res = await apis.getPlayList(userIdx);
-      console.log(res);
       dispatch(get_playList(res));
     } catch (error) {
       console.log("setPlayList Error : ", error);
