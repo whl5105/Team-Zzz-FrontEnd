de7687d1990be0ef32cc0c4a61b727518c574dfa
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2022-01-18 00:58:52 +0900

    [add] NoRecord > NoInfo 이미지 추가

--- src/components/diary/NoRecord.js
+++ src/components/diary/NoRecord.js
@@ -2,11 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 // --- images ---
-import NoRecord_B_1 from "../../static/images/diary/NoRecord_B_1.png";
-import NoRecord_B_2 from "../../static/images/diary/NoRecord_B_2.png";
-import NoRecord_B_3 from "../../static/images/diary/NoRecord_B_3.png";
-import NoRecord_R from "../../static/images/diary/NoRecord_R.png";
-import NoRecord_T from "../../static/images/diary/NoRecord_T.png";
+import NoInfo from "../../static/images/diary/NoInfo.png";
 
 const NoRecord = (props) => {
   return (
@@ -24,11 +20,10 @@ const Wrap = styled.div`
 const Image = styled.div`
   width: 100%;
   height: inherit;
-  background: url(${NoRecord_T}), url(${NoRecord_B_1}), url(${NoRecord_B_2}),
-    url(${NoRecord_B_3}), url(${NoRecord_R});
-  background-size: 600px, 650px, 650px, 650px, 600px;
-  background-position: 20% 50%, 0% 80%, 0% 80%, 0% 80%, 150% 72%;
-  background-repeat: no-repeat; ;
+  background: url(${NoInfo});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 `;
 
 export default NoRecord;
