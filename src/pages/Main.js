faa6b2dd57271e0a098a578a1f2596356246ebe5
taeeon kim <myhom43@gmail.com>
2022-01-17 17:51:06 +0900

    [docs] 메인푸쉬알림로직변경, 구독수정중

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { history } from "../redux/configureStore";
+import axios from "axios";
 
 // --- components ---
 import FirstNotification from "../pages/FirstNotification";
@@ -13,18 +14,95 @@ import nature from "../static/images/banner/nature_1005px.png";
 import object from "../static/images/banner/object_1005px.png";
 import space from "../static/images/banner/space_1005px.png";
 
+// firebas
+import firebase from "firebase/compat/app"; //firebase모듈을 import해줘야 합니다.
+import { getMessaging, getToken } from "firebase/messaging";
+
+const config = {
+  apiKey: "AIzaSyD7vx1YcQDmd7Gom-mOGzB_j_oYD4qjR9M",
+  authDomain: "pushnotificationtest-9e21c.firebaseapp.com",
+  projectId: "pushnotificationtest-9e21c",
+  storageBucket: "pushnotificationtest-9e21c.appspot.com",
+  messagingSenderId: "1019872102596",
+  appId: "1:1019872102596:web:57ec3461348eca0ea1e191",
+  measurementId: "G-TFEDXNHVGY",
+};
+firebase.initializeApp(config);
+
+const messaging = getMessaging();
+
 const Main = (props) => {
+  //  console.log({...Notification.requestPermission})
+
+  Notification.requestPermission().then(function (result) {
+    console.log(result);
+    console.log(Notification.permission);
+
+    if (result === "granted") {
+      getToken(messaging, {
+        vapidKey:
+          "BHpAKY7pMnF5to1B-R9DDGRn5w6a5APBojAnwVr1ZyW56w4sPQGqIoCZphWfSHyohcOmKeuvvJHPj8B2KAZT4Ko",
+      })
+        .then((currentToken) => {
+          if (currentToken) {
+            console.log("알림 O");
+            console.log(currentToken);
+            permission = true;
+            console.log(noticeSet, token, mobile, permission);
+
+            if (!noticeSet && token && !mobile && permission) {
+              setNoticationModal(true);
+            }
+
+            history.test = currentToken;
+            console.log(history);
+            // axios
+            //   .post(
+            //     "https://fcm.googleapis.com/fcm/send",
+            //     {
+            //       "notification": {
+            //         "body": "새로운글",
+            //         "title": "ㅇㅇdddddd",
+            //       },
+            //       "to": currentToken,
+            //     },
+            //     {
+            //       headers: {
+            //         "Content-type": "application/json",
+            //         "Authorization": "key=AAAA7XUdSMQ:APA91bHiG3ONselw3DtnFO6-7Z2hPZq_qh9zQihBUnkrpebWvTNvSv1J8d5jQI4RgH3b7wXXlwQoQSTytd_lvwnFBeVkyV3-ShUa0HL_mpmcuBckF5bLlxhDertxC8YsONjZVntYrCk2",
+            //       },
+            //     }
+            //   )
+            //   .then((res) => {
+            //     console.log(res.data, res.config.data);
+            //   });
+          } else {
+            console.log(
+              "No registration token available. Request permission to generate one."
+            );
+          }
+        })
+        .catch((err) => {
+          console.log("An error occurred while retrieving token. ", err);
+        });
+    } else if (result === "denied") {
+      console.log("권한 차단");
+    }
+  });
+
   function Mobile() {
     return /iPhone|iPad/i.test(navigator.userAgent);
   }
   const [mobile, setMobile] = React.useState(Mobile()); // IOS이면 true, 나머지는 false
   const [noticationModal, setNoticationModal] = React.useState(true);
+  let [permission, setPermission] = React.useState(false);
+  const token = localStorage.getItem("token");
+  const noticeSet = JSON.parse(localStorage.getItem("noticeSet"));
 
   React.useEffect(() => {
-    const noticeSet = JSON.parse(localStorage.getItem("noticeSet"));
-    const token = localStorage.getItem("token");
+    console.log(token, mobile, permission);
 
-    if (!noticeSet && token && !mobile) {
+    if (!noticeSet && token && !mobile && permission) {
       setNoticationModal(true);
     }
   }, []);
