656d28167c3597dcadd343b2bb674784e5d4f5e8
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2022-01-18 00:43:07 +0900

    [add, css] DropDown 컴포넌트 아이콘 추가, 수저

--- src/components/diary/NoRecord.js
+++ src/components/diary/NoRecord.js
@@ -2,8 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 // --- images ---
-import NoRecord_B from "../../static/images/diary/NoRecord_B.png";
+import NoRecord_B_1 from "../../static/images/diary/NoRecord_B_1.png";
+import NoRecord_B_2 from "../../static/images/diary/NoRecord_B_2.png";
+import NoRecord_B_3 from "../../static/images/diary/NoRecord_B_3.png";
 import NoRecord_R from "../../static/images/diary/NoRecord_R.png";
+import NoRecord_T from "../../static/images/diary/NoRecord_T.png";
 
 const NoRecord = (props) => {
   return (
@@ -21,10 +24,11 @@ const Wrap = styled.div`
 const Image = styled.div`
   width: 100%;
   height: inherit;
-  background: url(${NoRecord_B}), url(${NoRecord_R});
-  background-size: 685px, 600px;
-  background-position: 50% 100%, 49% 50%;
-  background-repeat: no-repeat;
+  background: url(${NoRecord_T}), url(${NoRecord_B_1}), url(${NoRecord_B_2}),
+    url(${NoRecord_B_3}), url(${NoRecord_R});
+  background-size: 600px, 650px, 650px, 650px, 600px;
+  background-position: 20% 50%, 0% 80%, 0% 80%, 0% 80%, 150% 72%;
+  background-repeat: no-repeat; ;
 `;
 
 export default NoRecord;
