23f1530edac455bd889ee22cb797b4819f59a0e8
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2022-01-21 23:41:03 +0900

    [fix] Asmr 음원 선택 오류 수정

--- src/pages/Asmr.js
+++ src/pages/Asmr.js
@@ -132,7 +132,7 @@ const Asmr = (props) => {
       songDelete(asmrUrl);
     } else {
       if (play.length > 3) {
-        window.alert("음원은 최대 4개까지만 담으실 수 있습니다.");
+        window.alert("음원은 최대 4개까지 담으실 수 있습니다.");
       } else {
         songSetting(asmrUrl, iconUrl, title);
         songSelect(asmrUrl);
